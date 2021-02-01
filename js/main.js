9007c6c0a2e392681ba4c78da5be63fe591c9dd2
PriyankaDulam <78066574+PriyankaDulam@users.noreply.github.com>
2021-02-01 15:47:42 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch("../data/data.json").then(response=>{
+fetch("https://priyankadulam.github.io/ecommerce/data/data.json").then(response=>{
     return response.json()
 }).then(data=>{
     displayData(data)
